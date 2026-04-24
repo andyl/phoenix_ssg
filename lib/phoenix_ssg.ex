8c7f8e6a70e26e7cf944c5fbe297471553d229d1
andyl <andy@r210.com>
2026-04-23 21:04:19 -0700

    chore: first commit

---
+++ lib/phoenix_ssg.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixSsg do
+  @moduledoc """
+  Documentation for `PhoenixSsg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixSsg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_ssg_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixSsgTest do
+  use ExUnit.Case
+  doctest PhoenixSsg
+
+  test "greets the world" do
+    assert PhoenixSsg.hello() == :world
+  end
+end
