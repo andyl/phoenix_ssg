642ad35bb8ce943940d4e0f6964abfb8fc1fa09d
andyl <andy@r210.com>
2026-04-25 20:16:18 -0700

    chore: add git_ops
    chore: asdf

--- mix.exs
+++ mix.exs
@@ -1,29 +1,28 @@
 defmodule PhoenixSsg.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
   def project do
     [
       app: :phoenix_ssg,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:commit_hook, path: "/home/aleak/src/Tool/commit_hook"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:git_ops, "~> 2.0", only: [:dev], runtime: false},
+      {:igniter, "~> 0.6", optional: true},
+      {:commit_hook, path: "/home/aleak/src/Tool/commit_hook"}
     ]
   end
 end
